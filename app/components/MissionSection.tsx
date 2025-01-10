6721daecd86500b4facb3311d78083584f39061a
harshmriduhash <harshsahay2709@gmail.com>
2025-01-10 22:59:10 +0530

    redoing

--- app/components/MissionSection.tsx
+++ app/components/MissionSection.tsx
@@ -2,10 +2,10 @@ export default function MissionSection() {
   return (
     <section id="about" className="container mx-auto text-left py-1 px-6">
       <h2 className="text-4xl font-bold text-left mb-10">
-        Why Choose StartifyAI?
+        Why Choose Startify-AI?
       </h2>
       <p className="text-xl text-gray-700">
-        At StartifyAI, our mission is to empower entrepreneurs and early-stage
+        At Startify-AI, our mission is to empower entrepreneurs and early-stage
         startups with AI-driven tools and insights.
       </p>
     </section>
